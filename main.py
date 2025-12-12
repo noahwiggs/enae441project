00069ae9177fad257a7dfcfeebb6973957ddeb31
Noah Wigglesworth <nwiggles@umd.edu>
2025-12-12 15:56:51 -0500

    main edits

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+X_oe = [7000, 0.2, 45, 0, 270, 78.75] 
+#a(km), e(dimless), i(deg), omega(deg), Omega(deg), theta(deg)
+
+DSN1_loc = [35.297, -116.914] #lat(deg), long(deg)
+DSN2_loc = [40.4311, -4.248]
+DSN3_loc = [-35.4023, -148.9813]
+
+R_Earth = 6378.137 #km
+omega_EN = 7.292115e-5 #rad/sec
+gamma_0 = 0 #deg
+
