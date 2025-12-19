20249f6a03b679fd5060cae5283e07a015683187
Vijay-Jetton <vjetton@umd.edu>
2025-12-19 01:02:56 -0500

    small changes

--- main.py
+++ main.py
@@ -167,19 +167,4 @@ plot_pure_prediction(results)
 from EKF import plot_with_updates
 plot_with_updates(results)
 
-plt.show()
-
-meas = np.load("Project-Measurements-Easy.npy")
-t = meas[:,0].astype(float)
-
-print("t min/max:", t.min(), t.max())
-print("any dt <= 0 ?", np.any(np.diff(t) <= 0))
-print("number of dt <= 0:", np.sum(np.diff(t) <= 0))
-
-bad = np.where(np.diff(t) <= 0)[0]
-print("first few bad indices:", bad[:10])
-if bad.size > 0:
-    i = bad[0]
-    print("t[i], t[i+1]:", t[i], t[i+1], "dt:", t[i+1]-t[i])
-    print("rows:\n", meas[i], "\n", meas[i+1])
 
